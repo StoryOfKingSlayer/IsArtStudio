15f388371167347ec000df41e5c7555b78c6a2a5
StoryOfKingSlayer <shura.prit@list.ru>
2021-05-29 22:54:34 +0300

    fird comm

--- tutorial/tutorial/quickstart/admin.py
+++ tutorial/tutorial/quickstart/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Project, Step,Task
+
+admin.site.register(Project)
+admin.site.register(Step)
+admin.site.register(Task)
