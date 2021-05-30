a9b1ba36e0b68b0c9a3a07ca1a9fafa5880e9183
StoryOfKingSlayer <shura.prit@list.ru>
2021-05-30 12:19:42 +0300

    forth comm

--- tutorial/tutorial/quickstart/admin.py
+++ tutorial/tutorial/quickstart/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Project, Step,Task
+from .models import Project, Step,Task, User
 
 admin.site.register(Project)
 admin.site.register(Step)
 admin.site.register(Task)
+admin.site.register(User)
